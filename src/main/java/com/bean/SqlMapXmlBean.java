1253e8ca04b929312cd2226473bc5aa5e8eeaa7f
wuzhq <awwzc@sina.com>
2015-11-09 16:32:30 +0800

    增加sqlmap-xml模板bean

---
+++ src/main/java/com/bean/SqlMapXmlBean.java
@@ -0,0 +1,14 @@
+package com.bean;
+/**
+ * ibtais sqlmap-xml 模板配置类
+ * @author wuzhq
+ *
+ */
+public class SqlMapXmlBean {
+	private JavaDtoCLassBean javaDtoCLassBean;
+	private String nameSpace;
+	private String aliasDtoClassName;
+	private String aliasDtoClassFullName;
+	private String aliasDtoOpConditionClassName;
+	private String aliasDtoOpConditionClassFullName;
+}
